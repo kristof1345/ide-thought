7bd3abaa1f99787356b381a5441f90792df536bb
kristof1345 <kristofnemeth1345@gmail.com>
2023-07-11 13:32:51 +0200

    ok

---
+++ src/firebaseConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import "dotenv/config";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOM,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE,
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEAS_ID,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const database = getFirestore(app);
