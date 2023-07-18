3a8c662e0699b5f3df2db6a617fd6e900c53faf2
kristof1345 <kristofnemeth1345@gmail.com>
2023-07-18 12:48:10 +0200

    ok

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,14 +6,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log(import.meta.env.VITE_API_KEY);
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOM,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE,
-  messagingSenderId: process.env.SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEAS_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOM,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE,
+  messagingSenderId: import.meta.env.VITE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEAS_ID,
 };
 
 // Initialize Firebase
